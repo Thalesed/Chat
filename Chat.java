91143b9b51e53097fb64b1e6e9f402a3f2b6d736
Thalesed <104565512+Thalesed@users.noreply.github.com>
2023-04-05 09:18:06 -0300

    Update Chat.java

--- Chat.java
+++ Chat.java
@@ -1,18 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+import java.io.IOException;
+import java.net.InetAddress;
 
-package com.mycompany.chat;
-
-/**
- *
- * @author alunos
- */
 public class Chat {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         Enter tela = new Enter();
         tela.setLocationRelativeTo(null);
         tela.setVisible(true);
+
     }
 }
