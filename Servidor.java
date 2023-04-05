98a3db899380e3871fe1a274a2fd70452e101891
Thalesed <104565512+Thalesed@users.noreply.github.com>
2023-04-05 09:15:49 -0300

    Add files via upload

---
+++ Servidor.java
@@ -0,0 +1,32 @@
+
+
+import java.io.IOException;
+import java.net.*;
+
+public class Servidor {
+    private ServerSocket server;
+
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverS = new ServerSocket(6969);
+        Servidor server = new Servidor(serverS);
+        server.start();
+    }
+
+    public Servidor(ServerSocket server){
+        this.server = server;
+    }
+
+    public void start() throws IOException{
+        while(!server.isClosed()){
+            Socket cliente = server.accept();
+            System.out.println("Novo cliente");
+            new TratamentoClasse(cliente).start();
+        }
+    }
+
+    public void close() throws IOException{
+        if(server!=null){
+            server.close();
+        }
+    }
+}
