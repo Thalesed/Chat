9b87fdc4302a3b1ec064751dde9e9b1368b54666
Thalesed <104565512+Thalesed@users.noreply.github.com>
2023-04-05 09:19:06 -0300

    Update Recebedor.java

--- Recebedor.java
+++ Recebedor.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.chat;
 
-/**
- *
- * @author alunos
- */
 import java.io.InputStream;
 import java.util.Scanner;
 public class Recebedor implements Runnable {
@@ -20,13 +11,10 @@ public class Recebedor implements Runnable {
  
    @Override
    public void run() {
-     // recebe msgs do servidor e imprime na tela
      Scanner s = new Scanner(this.servidor);
      while (s.hasNextLine()) {
          String line  = s.nextLine();
        System.out.println(line);
-       //Interface.textArea.setLineWrap(rootPaneCheckingEnabled);
-       //Interface.textArea.setWrapStyleWord(rootPaneCheckingEnabled);
        Interface.textArea.setText(Interface.textArea.getText() + line + "\n");
      }
    }
